ab4a3a55f054aa536c51ee86ede6294297feed70
Dan Burton <danburton.email@gmail.com>
2020-09-25 19:07:04 -0400

    haddock everything

--- sbin/build-all.sh
+++ sbin/build-all.sh
@@ -5,4 +5,21 @@
 RESOLVER="$1"
 PACKAGES_FILE="$2"
 
+if [ -f stack.yaml ] ; then
+  mv stack.yaml stack.yaml.bak
+fi
+
+echo '# written by build-all.sh
+resolver: $RESOLVER
+packages: []
+ghc-options:
+  "$everything": -haddock
+' > stack.yaml
+
 xargs stack --resolver "$RESOLVER" build < "$PACKAGES_FILE" 
+
+if [ -f stack.yaml.bak ] ; then
+  mv stack.yaml.bak stack.yaml
+else
+  rm stack.yaml
+fi
